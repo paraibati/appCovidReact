8d6766f7ebbd8c30fae94d16ff5e51d93cb1aa69
rlcarneiro97 <rafaeldelima97@hotmail.com>
2020-07-22 10:49:58 -0300

    tela de login criada

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -2,6 +2,8 @@ import React, { Component} from "react"
 // import Api from "../../services/api"
 import "./style.css"
 import {Link} from "react-router-dom"
+import { toast } from 'react-toastify';
+const Logo = require("../../assets/logo.png")
 
 export default class Login extends Component{
     
@@ -9,13 +11,43 @@ export default class Login extends Component{
         super()
 
         this.state = {
-
+            email: "",
+            password: ""
         }
+
+        this.emailChange = this.emailChange.bind(this)
+        this.passwordChange = this.passwordChange.bind(this)
+        this.handleSubmit = this.handleSubmit.bind(this)
     }
 
     // metodo pra preencher o objeto todo
-    componentDidMount(){
+    // componentDidMount(){
 
+    // }
+
+    emailChange(event){
+        this.setState({
+            email: event.target.value
+        })
+    }
+
+    passwordChange(event){
+        this.setState({
+            password: event.target.value
+        })
+    }
+
+    handleSubmit(event){
+        const {email, password} = this.state
+
+        if(email !== "" && password !== ""){
+            toast.success("Login efetuado com sucesso!", {position: toast.POSITION.BOTTOM_CENTER})
+            this.props.history.push("/home")
+        }else{
+            toast.error("Por favor, Preencha Todos os Campos!", {position: toast.POSITION.BOTTOM_CENTER})
+        }
+
+        event.preventDefault(event)
     }
 
     // metodo para renderizar a pagina
@@ -23,9 +55,20 @@ export default class Login extends Component{
         
         return (
             <div className="login">
-                <p>Login</p>
+                <div className="logo">
+                    <img src={Logo} alt="erro no carregamento da imagem"/>
+                </div>
+                <article>
+                    <form onSubmit={this.handleSubmit}>
+                        <input type="text" placeholder="Seu e-mail" value={this.state.value} onChange={this.emailChange}/>
+                        <input type="password" placeholder="Sua senha" value={this.state.value} onChange={this.passwordChange}/>
+                        <input type="submit" value="Acessar"/>
+                        <Link to={"/cadastro"}>Você ainda não tem uma conta?<br></br>Clique aqui para criar uma agora!</Link>
+                    </form>    
+                </article>
+                {/* <p>Login</p>
                 <Link to={"/cadastro"}>Cadastro</Link>
-                <Link to={"/home"}>Home</Link>
+                <Link to={"/home"}>Home</Link> */}
             </div>
         )
     }
