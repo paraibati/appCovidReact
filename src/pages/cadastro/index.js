d36f0b2feda7c97fbbd90e61ed4b15e9863f35d4
rlcarneiro97 <rafaeldelima97@hotmail.com>
2020-07-22 11:24:04 -0300

    retirando metodos desnecessarios e criacao da tela de cadastro

--- src/pages/cadastro/index.js
+++ src/pages/cadastro/index.js
@@ -2,6 +2,8 @@ import React, { Component} from "react"
 // import Api from "../../services/api"
 import "./style.css"
 import {Link} from "react-router-dom"
+import { toast } from 'react-toastify';
+const Logo = require("../../assets/logo.png")
 
 export default class Cadastro extends Component{
     
@@ -9,25 +11,65 @@ export default class Cadastro extends Component{
         super()
 
         this.state = {
-
+            name: "",
+            email: "",
+            password: ""
         }
+
+        this.nameChange = this.nameChange.bind(this)
+        this.emailChange = this.emailChange.bind(this)
+        this.passwordChange = this.passwordChange.bind(this)
+        this.handleSubmit = this.handleSubmit.bind(this)
     }
 
-    // metodo pra preencher o objeto todo
-    componentDidMount(){
+    nameChange(event){
+        this.setState({
+            name: event.target.value
+        })
+    }
 
+    emailChange(event){
+        this.setState({
+            email: event.target.value
+        })
+    }
+
+    passwordChange(event){
+        this.setState({
+            password: event.target.value
+        })
+    }
+
+    handleSubmit(event){
+        const {name, email, password} = this.state
+
+        if(name !== "" && email !== "" && password !== ""){
+            toast.success("Cadastro efetuado com sucesso!", {position: toast.POSITION.BOTTOM_CENTER})
+            this.props.history.push("/home")
+        }else{
+            toast.error("Por favor, Preencha Todos os Campos!", {position: toast.POSITION.BOTTOM_CENTER})
+        }
+
+        event.preventDefault(event)
     }
 
     // metodo para renderizar a pagina
     render(){
         
         return (
-            <div>
-                <div className="cadastro">
-                <p>Cadastro</p>
-                <Link to={"/"}>Login</Link>
-                <Link to={"/home"}>Home</Link>
-            </div>
+            <div className="cadastro">
+                <div className="logo">
+                    <img src={Logo} alt="erro no carregamento da imagem"/>
+                </div>
+                <article>
+                    <form onSubmit={this.handleSubmit}>
+                        <input type="text" placeholder="Nome Completo" value={this.state.value} onChange={this.nameChange}/>
+                        <input type="text" placeholder="Seu e-mail" value={this.state.value} onChange={this.emailChange}/>
+                        <input type="password" placeholder="Sua senha" value={this.state.value} onChange={this.passwordChange}/>
+                        <input type="submit" value="Criar conta"/>
+                        <Link to={"/"}>Já tenho uma conta</Link>
+                    </form>    
+                </article>
             </div>
         )
     }
